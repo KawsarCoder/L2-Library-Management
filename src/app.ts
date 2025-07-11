66398c1ae2a65a8fa73deeefc0e58d32f041ed3b
KawsarCoder <md.kawsar.developer@gmail.com>
2025-07-12 00:49:15 +0600

    dotenv config organised

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,21 @@
 import express, { Application, Request, Response } from "express";
 import { booksRoutes } from "./app/controllers/book.controller";
+import { errorhandler } from "./app/middlewares/errorHandler";
+import { notFoundHandler } from "./app/middlewares/notFoundHandler";
 
 const app: Application = express();
 app.use(express.json());
 
-app.use("/api/books", booksRoutes);
+// app.use("/api/books", booksRoutes);
 
+app.use("/books", booksRoutes);
+app.use("/borow", booksRoutes);
+
+app.use(errorhandler);
+app.use(notFoundHandler);
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Welcome to Node app!");
+  res.send("Welcome to library management app!");
 });
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -2,22 +2,17 @@ import { Server } from 'http';
 import mongoose from 'mongoose';
 import app from './app';
 import dotenv from 'dotenv';
-
-dotenv.config();
+import config from './config';
 
 let server: Server;
-const PORT = process.env.PORT || 5000;
 
 async function main() {
   try {
-    await mongoose.connect(
-      process.env.DATABASE_URL ||
-      process.env.LOCAL_DATABASE_URL ||
-      'mongodb://localhost:27017/library-management-db');
+    await mongoose.connect(config.DATABASE_URL);
     console.log('Database connected successfully');
 
-    server = app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
+    server = app.listen(config.PORT, () => {
+      console.log(`Server is running on port ${config.PORT}`);
     });
   } catch (error) {
     console.log('Database connection failed:', error);
